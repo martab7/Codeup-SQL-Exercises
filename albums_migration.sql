236a0b934a45c1dfab3938f3d5fa2963f4ec1a52
Anthony <martab7.am@gmail.com>
2016-07-25 15:02:49 -0500

    First sql table created with script titled Albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,11 @@
 USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(125) NOT NULL,
+  name VARCHAR(150) NOT NULL,
+  release_date DATE NOT NULL,
+  sales FLOAT NOT NULL,
+  genre VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+)
