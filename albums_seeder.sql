7f2fab707bc8bf779edae9803bc59d5a04317299
Anthony <martab7.am@gmail.com>
2016-07-25 16:16:57 -0500

    Adds feeder filer to albums database

---
+++ albums_seeder.sql
@@ -0,0 +1,7 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', '1982-00-00', 45.4, 'Pop, rock, R&B'),
+('AC/DC', 'Back in Black', '1980-00-00', 25.0, 'Hard rock'),
+('Pink Floyd', 'The Dark Side of the Moon', '1973-00-00', 22.7, 'Progressive rock'),
+('Whitney Houston', 'The Bodyguard', '1992-00-00', 27.4, 'Soundtrack/R&B, soul, pop'),
+('Meat Loaf', 'Bat Out of Hell', '1977-00-00', 20.6, 'Hard rock, progressive rock');
