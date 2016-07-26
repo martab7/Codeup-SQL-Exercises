1d851740ff828b177321a7c6ce2228cb637cd8dc
Anthony <martab7.am@gmail.com>
2016-07-26 14:28:11 -0500

    Creates delete sql file to delete certain entries of albums table

---
+++ delete_exercises.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+SELECT * FROM albums WHERE release_date > '1991-00-00';
+
+SELECT * FROM albums WHERE genre = 'Hard rock';
+
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+Delete from albums where release_date > '1991-00-00';
+
+DELETE FROM albums WHERE genre = 'Hard rock';
+
+DELETE FROM albums where artist = 'Pink Floyd';
