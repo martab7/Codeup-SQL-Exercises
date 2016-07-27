c50a2cb470c2f3813924754d1e979302670be8cc
Anthony <martab7.am@gmail.com>
2016-07-27 11:05:00 -0500

    Updates file to include more where clauses with in, or, and and

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,3 +3,11 @@ SELECT last_name from employees where last_name like 'E%';
 SELECT hire_date from employees where hire_date between '1990-01-01' and '1999-12-31';
 SELECT birth_date from employees where birth_date like '%-12-25';
 SELECT last_name from employees where last_name like '%q%';
+
+-- Where clauses Continued
+SELECT first_name from employees where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+SELECT first_name from employees where gender = 'M' and (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya');
+SELECT last_name from employees where last_name like ('E%') or last_name like ('%e');
+SELECT last_name from employees where last_name like ('E%') and last_name like ('%e');
+SELECT * from employees where hire_date between '1990-01-01' and '1999-12-31' and birth_date like '%-12-25';
+SELECT last_name from employees where last_name like '%q%' and last_name not like '%qu%';
